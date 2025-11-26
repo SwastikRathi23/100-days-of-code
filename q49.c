186636a5559f3d5ef586aedcb0847748194233ef
SwastikRathi23 <swastik.rathi23@gmail.com>
2025-11-27 00:30:52 +0530

    Add files via upload

---
+++ q49.c
@@ -0,0 +1,21 @@
+//Write a program to print the following pattern:
+//5
+//45
+//345
+//2345
+//12345
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int rows = 5;  
+
+    for (i = 0; i < rows; i++) {            
+        for (j = rows - i; j <= 5; j++) {       
+            printf("%d", j);
+        }
+        printf("\n");  
+    }
+
+    return 0;
+}
---
+++ q50.c
@@ -0,0 +1,26 @@
+//Write a program to print the following pattern:
+//*****
+// ****
+//  ***
+//   **
+//    *
+#include <stdio.h>
+
+int main() {
+    int i, j;
+    int rows = 5;  
+
+    for (i = 1; i <= rows; i++) {
+    
+        for (j = 1; j < i; j++) {
+            printf(" ");
+        }
+        
+        for (j = 1; j <= rows - i + 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
